56271bb8d19c1c42dfbc72e1a19bcaad9451ff85
AGTHARN <63234276+AGTHARN@users.noreply.github.com>
2020-05-26 07:13:10 +0800

    D:

--- src/AGTHARN/FlyPE/Main.php
+++ src/AGTHARN/FlyPE/Main.php
@@ -49,6 +49,7 @@ class Main extends PluginBase implements Listener {
 	    if($configversion < "2"){
 		    $this->getLogger()->warning("Your config is outdated! Please delete your old config to get the latest features!");
 		    $this->getServer()->getPluginManager()->disablePlugin($this);
+		    //im sorry dylan
 	    }
     }
 
